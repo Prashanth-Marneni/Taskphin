ac414d18289bf20cf829af0433f337c0e09eb221
Prashanth-Marneni <prashanthmarneni10@gmail.com>
2024-03-21 00:07:43 +0530

    Changed PORT

--- backend/index.js
+++ backend/index.js
@@ -116,7 +116,7 @@ app.get("/candidates", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
